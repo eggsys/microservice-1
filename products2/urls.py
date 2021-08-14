43155fc90f65a8207ab72bf41c54beb6d7b4e828
eggsys <egg.rkt2@gmail.com>
2021-08-14 16:30:18 +0700

    ise rest_framework responsce instend

--- products2/urls.py
+++ products2/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import ProductViewSet
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post':'create'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'post':'update',
+        'delete': 'destroy'
+    })),
+
+
 ]
--- products2/views.py
+++ products2/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+#from django.http import response
+#from admin.products2.models import Product
+#from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        print("productViewset:LIST")
+        print(serializer.data)
+        print("END !")
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, reuqest, pk=None):
+        pass
